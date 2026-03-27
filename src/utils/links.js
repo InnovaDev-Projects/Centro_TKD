abf9825258fa385ce3de7acd4eeb49a43ba71412
Mendoza Matias <mendoza.e.matias@gmail.com>
2026-03-27 15:01:36 -0300

    create package utils

---
+++ src/utils/links.js
@@ -0,0 +1,8 @@
+export const LINKS = {
+    whatsapp: "https://api.whatsapp.com/send/?phone=1144353469&text&type=phone_number&app_absent=0",
+    instagram: "https://www.instagram.com/centro.tkd/",
+    facebook: "https://www.facebook.com/profile.php?id=100086311917381&sk=about",
+    youtube: "https://www.youtube.com/@Centrotkd",
+    maps: "https://www.google.com/maps?q=Centro+TKD+-+Taekwondo+%26+Fitness,+Pres.+Juan+Domingo+Per%C3%B3n+1241,+B1744FKC+Gran+Buenos+Aires,+Provincia+de+Buenos+Aires",
+    mapsEmbed: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3283.5!2d-58.6!3d-34.6!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bc95004ae03313%3A0xdcb99955666c6809!2sCentro%20TKD%20-%20Taekwondo%20%26%20Fitness!5e0!3m2!1ses-419!2sar!4v1700000000000",
+};
